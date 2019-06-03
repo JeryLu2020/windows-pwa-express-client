100d26ec400ba9936bef3f2cbde9400aeccad201
13127755587 <13127755587@163.com>
2019-06-04 02:23:10 +0800

    update

--- routes/account.js
+++ routes/account.js
@@ -4,8 +4,13 @@ var router = express.Router();
 // const userServices  = require('../config/users-services');
 
 router.get('/', (req,res)=>{
-    res.render('account', { layout: 'layout', title: 'VIP'});
+    res.render('account');
 });
+
+router.get('/create', (req,res)=>{
+    res.render('account-create');
+});
+
 
 
 
