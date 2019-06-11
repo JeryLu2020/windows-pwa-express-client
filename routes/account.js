43bc076a378b1a47deaee21fa1d41d606f1dfab5
13127755587 <13127755587@163.com>
2019-06-12 00:20:36 +0800

    fixed country and state

--- routes/account.js
+++ routes/account.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
-// const userServices  = require('../config/users-services');
+var country = require('countryjs');
 
 router.get('/', (req,res)=>{
     res.render('account');
 });
 
 router.get('/create', (req,res)=>{
-    // read contry-state data from local and 
     res.render('account-create');
+});
+
+router.get('/create/details', (req,res)=>{
+    res.render('account-create-details');
+});
+
+router.post('/create/get_state', (req, res)=> {
+    var states = country.states(req.body.country_name);
+    if(states) {
+        return res.send(states);
+    } else {
+        return res.render('error', { errmsg: err });
+    }
+});
+
+router.get('/create/get_city', (req, res)=>{
+
 });
 
 router.get('/create/email', (req,res)=>{
@@ -17,6 +32,8 @@ router.get('/create/email', (req,res)=>{
 });
 
 
-
-
 module.exports = router;
+
+
+// get state by country https://www.npmjs.com/package/countryjs
+// get city by state
